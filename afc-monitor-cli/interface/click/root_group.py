e2abfb2fcae27f2ee2e21e2f124cfa27bb31cad1
Henry Hsieh <hhsieh@wi-fi.org>
2024-05-29 19:02:25 +0800

    Update ASCII Art configuration

--- afc-monitor-cli/interface/click/root_group.py
+++ afc-monitor-cli/interface/click/root_group.py
@@ -1,11 +1,18 @@
 import click
-from art import text2art, tprint
 
 from interface.click.command.proactive_monitor_command import proactive_monitor_command
 
 
 # Banner art text
-banner_art_text = tprint("AFC Monitor CLI")
+banner_art_text = """
+\b
+    ___    ____________   __  ___            _ __                ________    ____
+   /   |  / ____/ ____/  /  |/  /___  ____  (_) /_____  _____   / ____/ /   /  _/
+  / /| | / /_  / /      / /|_/ / __ \/ __ \/ / __/ __ \/ ___/  / /   / /    / /  
+ / ___ |/ __/ / /___   / /  / / /_/ / / / / / /_/ /_/ / /     / /___/ /____/ /   
+/_/  |_/_/    \____/  /_/  /_/\____/_/ /_/_/\__/\____/_/      \____/_____/___/   
+                                                                                 
+"""
 
 # Root Group
 @click.group(help=banner_art_text)
