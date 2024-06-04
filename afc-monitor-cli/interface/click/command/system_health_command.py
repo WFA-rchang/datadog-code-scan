86908fa14cb5c18b02bb72a8757ea40ad7fa08b1
Ray Chang <rchang@wi-fi.org>
2024-06-04 16:55:54 +0800

    Remove default env from system health command

--- afc-monitor-cli/interface/click/command/system_health_command.py
+++ afc-monitor-cli/interface/click/command/system_health_command.py
@@ -7,10 +7,10 @@ from application.system_health_implementation import SystemHealthApplicationImpl
 
 @click.command(name='system-health', help="Execute System Health Check")
 @click.option('--system-names', '-s', help="System names to check, string separated by comma, default: all systems")
-@click.option('--env', '-e', help="Environment to check system health, default: staging", required=True)
+@click.option('--env', '-e', help="Environment to check system health", required=True)
 @inject
 def system_health_command(system_health_application: SystemHealthApplicationImplementation = Provide[Container.system_health_application],
-                          system_names: str = '', env: str = 'staging'):
+                          system_names: str = '', env: str = ''):
     # Get System Healths
     click.echo("- Getting System Healths -")
     system_name_list = system_names.split(',') if system_names != None else []
