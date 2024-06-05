078c9a6218454b8fbba5b5cbf202549d48b78be6
Ray Chang <rchang@wi-fi.org>
2024-06-05 13:39:50 +0800

    Create scheduler_status command

---
+++ afc-monitor-cli/interface/click/command/scheduler_status_command.py
@@ -0,0 +1,31 @@
+import click
+from typing import List
+from dependency_injector.wiring import Provide, inject
+
+from infrastructure.dependency_injection.container import Container
+from application.scheduler_status_application_implementation import SchedulerStatusApplicationImplementation
+
+
+@click.command(name='scheduler-status', help="Execute Scheduler Execution Status Check")
+@click.option('--scheduler-names', help="Execute Scheduler Execution Status Check")
+@click.option('--env', '-e', help="Environment to check system health", required=True)
+@inject
+def scheduler_status_command(scheduler_status_application: SchedulerStatusApplicationImplementation = Provide[Container.scheduler_status_application],
+                          scheduler_names:str='', env: str = ''):
+    # Get Scheduler Status 
+    click.echo("- Getting Scheduler Status -")
+
+    scheduler_names_list = []
+    if scheduler_names != None:
+        scheduler_names_list = scheduler_names.split(',')
+
+    error, scheduler_status = scheduler_status_application.get_scheduler_status(scheduler_names=scheduler_names_list, env=env)
+
+    if error is not None:
+        raise click.ClickException(error)
+
+    for scheduler in scheduler_status:
+        click.echo(
+            f"Scheduler Name: {scheduler.name}, Status: {scheduler.status}"
+        )
+    click.echo("- End of Scheduler Status -")
--- afc-monitor-cli/interface/click/root_group.py
+++ afc-monitor-cli/interface/click/root_group.py
@@ -1,6 +1,7 @@
 import click
 
 from interface.click.command.system_health_command import system_health_command
+from interface.click.command.scheduler_status_command import scheduler_status_command
 from interface.click.command.proactive_monitor_command import proactive_monitor_command
 
 
@@ -24,3 +25,4 @@ def root_cli_group():
 # Add commands to Root Group
 root_cli_group.add_command(proactive_monitor_command)
 root_cli_group.add_command(system_health_command)
+root_cli_group.add_command(scheduler_status_command)
